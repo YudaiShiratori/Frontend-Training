674bb36c917123f0059b50fe8a062624e08592c2
YudaiShiratori <toritorikebabu1023x@gmail.com>
2019-01-29 12:29:56 +0900

    kadai2_todolist form_function

--- kadai2/src/App.vue
+++ kadai2/src/App.vue
@@ -1,64 +1,9 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div>
-      <input type="text" v-model="name">
-      <span v-once>I am {{ name }}</span>
-    </div>
-    
-    <div>
-      <div v-if="isShow">Can you see?</div>
-      <span v-for="(item, index) in items" :key="index">{{item }}</span>
-    </div>
-
-    <div v-for="(item) in infos" :key="item.id">
-      {{ item.name }}は{{ item.hobby}} が趣味
-    </div>
-    
-    <div @click="onClick(5)">
-      <button @click.stop="onClick(3)">
-        button
-      </button>
-      <p>{{ btnTag }}</p>
-    </div>
-
-    <div>
-      <a @click="onLink">google</a>
-    </div>
-
-    <div>
-      <input type="text" v-model="name">
-      <div>your name is {{ name }}</div>
-      <div>文字数: {{ nameCount }}</div>
-    </div>
-
-    <div>
-      <input type="text" placeholder="id" v-model="info.id"/>
-      <input type="text" placeholder="name" v-model="info.name"/>
-      <input type="text" placeholder="hobby" v-model="info.hobby"/>
-      <p>{{ watchItem }}</p>
-    </div>
-
-    <div v-if="dateJudgement">2019年ですね！</div>
-    <div v-bind:class="fontColor">エラーだったら赤色になるよ</div>
-
-    <div>{{ nowDate | year }}</div>
-    
-    <input-form v-model="info"/>
-    <div>
-      呼び元の値
-      <p>{{ info.name }}</p>
-      <p>{{ info.sex }}</p>
-      <p>{{ info.profile }}</p>
-    </div>
-  </div>
+  <input-form />  
 </template>
 
 <script>
-
-import InputForm from
-'./components/InputForm.vue'
+import InputForm from './components/InputForm.vue'
 
 export default {
   name: 'app',
@@ -67,77 +12,160 @@ export default {
   },
   data() {
     return {
-      name: 'aaa',
-      isShow: false,
-      items: ["a", "s", "h"],
-      btnTag: 0,
-      nameCount: 0,
-      info: {
-        id: 0, name: "asdf", hobby: "kyudo",
-        sex: "male", profile: "I like kyudo"
-      },
-      watchItem: "",
-      nowDate: new Date()
-    }
-  },
-  methods: {
-    onClick(tag) {
-      this.btnTag = tag
-    },
-    onLink() {
-      location.href = "https://www.google.com/"
-    }
-  },
-  created () {
-    console.log("DOM NO")
-  },
-  mounted () {
-    console.log("DOM OK")
-    this.nameCount = this.name.length
-  },
-  watch: {
-    name(newVal, oldVal) {
-      this.nameCount = newVal.length
-    },
-    'info.name': {
-      handler(val) {
-        this.watchItem = val
-      },
-      deep: true
-    }
-  },
-  computed: {
-    dateJudgement() {
-      const date = new Date()
-      return date.getFullYear() === 2019? true : false
-    },
-    fontColor() {
-      return this.error === null? "fontColor_blue" : "fontColor_red"
-    },
-    filters: {
-      year(val) {
-        return val.getFullYear()
-      }
+
     }
   }
-
 }
 </script>
 
-<style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
 
-.fontColor__blue {
-  color: blue
-}
-.fontColor__red {
-  color: red
-}
 </style>
+
+
+
+<!--
+// <template>
+//   <div id="app">
+//     <img alt="Vue logo" src="./assets/logo.png">
+//     <HelloWorld msg="Welcome to Your Vue.js App"/> 
+//     <div>
+//       <input type="text" v-model="name">
+//       <span v-once>I am {{ name }}</span>
+//     </div>
+    
+//     <div>
+//       <div v-if="isShow">Can you see?</div>
+//       <span v-for="(item, index) in items" :key="index">{{item }}</span>
+//     </div>
+
+//     <div v-for="(item) in infos" :key="item.id">
+//       {{ item.name }}は{{ item.hobby}} が趣味
+//     </div>
+    
+//     <div @click="onClick(5)">
+//       <button @click.stop="onClick(3)">
+//         button
+//       </button>
+//       <p>{{ btnTag }}</p>
+//     </div>
+
+//     <div>
+//       <a @click="onLink">google</a>
+//     </div>
+
+//     <div>
+//       <input type="text" v-model="name">
+//       <div>your name is {{ name }}</div>
+//       <div>文字数: {{ nameCount }}</div>
+//     </div>
+
+//     <div>
+//       <input type="text" placeholder="id" v-model="info.id"/>
+//       <input type="text" placeholder="name" v-model="info.name"/>
+//       <input type="text" placeholder="hobby" v-model="info.hobby"/>
+//       <p>{{ watchItem }}</p>
+//     </div>
+
+//     <div v-if="dateJudgement">2019年ですね！</div>
+//     <div v-bind:class="fontColor">エラーだったら赤色になるよ</div>
+
+//     <div>{{ nowDate | year }}</div>
+    
+//     <input-form v-model="info"/>
+//     <div>
+//       呼び元の値
+//       <p>{{ info.name }}</p>
+//       <p>{{ info.sex }}</p>
+//       <p>{{ info.profile }}</p>
+//     </div>
+//   </div>
+// </template>
+
+// <script>
+
+// import InputForm from
+// './components/InputForm.vue'
+
+// export default {
+//   name: 'app',
+//   components: {
+//     InputForm
+//   },
+//   data() {
+//     return {
+//       name: 'aaa',
+//       isShow: false,
+//       items: ["a", "s", "h"],
+//       btnTag: 0,
+//       nameCount: 0,
+//       info: {
+//         id: 0, name: "asdf", hobby: "kyudo",
+//         sex: "male", profile: "I like kyudo"
+//       },
+//       watchItem: "",
+//       nowDate: new Date()
+//     }
+//   },
+//   methods: {
+//     onClick(tag) {
+//       this.btnTag = tag
+//     },
+//     onLink() {
+//       location.href = "https://www.google.com/"
+//     }
+//   },
+//   created () {
+//     console.log("DOM NO")
+//   },
+//   mounted () {
+//     console.log("DOM OK")
+//     this.nameCount = this.name.length
+//   },
+//   watch: {
+//     name(newVal, oldVal) {
+//       this.nameCount = newVal.length
+//     },
+//     'info.name': {
+//       handler(val) {
+//         this.watchItem = val
+//       },
+//       deep: true
+//     }
+//   },
+//   computed: {
+//     dateJudgement() {
+//       const date = new Date()
+//       return date.getFullYear() === 2019? true : false
+//     },
+//     fontColor() {
+//       return this.error === null? "fontColor_blue" : "fontColor_red"
+//     },
+//     filters: {
+//       year(val) {
+//         return val.getFullYear()
+//       }
+//     }
+//   }
+
+// }
+// </script>
+
+// <style scoped>
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+
+// .fontColor__blue {
+//   color: blue
+// }
+// .fontColor__red {
+//   color: red
+// }
+// </style>
+-->
--- kadai2/src/components/InputForm.vue
+++ kadai2/src/components/InputForm.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="inputForm">
-    <p><input type="text" placeholder="名前" v-model="info.name"/></p>
-    <p>
-      <input type="radio" value="男性" v-model="info.sex" checked/>男性
-      <input type="radio" value="女性" v-model="info.sex"/>女性
-    </p>
-    <p><textarea rows="5" cols="30" placeholder="自己紹介" v-model="info.profile"/></p>
-    <button @click="onInit">初期化</button>
-    <button @click="onClick">表示</button>
-    <div v-if="isShow">
-      <p>{{ info.name }}</p>
-      <p>{{ info.sex }}</p>
-      <p>{{ info.profile }}</p>
-    </div>
+    <form @submit.prevent>
+      <input type="text" v-model="newTask">
+      <button @click="addTask">Add</button>
+      <button @click="deleteTask(index)">Delete</button>
+    </form>
+    <ul>
+      <li v-for="(todo, index) in todos">
+        <span v-toggle="checked">
+          <input type="checkbox" v-model="todo.checked">
+          {{ todo.task }}
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,48 +21,106 @@ export default {
   name: 'InputForm',
   data() {
     return {
-      isShow: false,
-      info: {
-        name: "",
-        sex: "男性",
-        profile: ""
-      }
-    }
-  },
-  watch: {
-    info: {
-      handler() {
-        this.isShow = false
-      },
-      deep: true
+      newTask: '',
+      todos: []
     }
   },
   methods: {
-    onClick() {
-      this.isShow = true
+    addTask: function(){
+      var todo = {
+        task : this.newTask,
+        checked: false
+      }
+      this.todos.push(todo)
+      this.newTask = ''
     },
-    OnInit() {
-      const value = {
-        name: "",
-        sex: "",
-        profile: ""
-      } 
-      this.$emit("input", value)
-    }
-  },
-  props: {
-    value: {
-      type: Object,
-      default: null
+    deleteTask: function(index){
+      alert(index)
+      if this.todo.checked
+        this.todos.splice(this.todo.index, 1)
     }
   }
 }
 </script>
 
-<style scoped>
-.inputForm {
-  margin: 10px;
-  background: wheat;
-}
+<style>
+  ul li {
+    list-style: none;
+  }
 </style>
 
+
+
+
+
+
+
+<!--
+// <template>
+//   <div class="inputForm">
+//     <p><input type="text" placeholder="名前" v-model="info.name"/></p>
+//     <p>
+//       <input type="radio" value="男性"v-model="info.sex" checked/>男性
+//       <input type="radio" value="女性" v-model="info.sex"/>女性
+//     </p>
+//     <p><textarea rows="5" cols="30" placeholder="自己紹介" v-model="info.profile"/></p>
+//     <button @click="onInit">初期化</button>
+//     <button @click="onClick">表示</button>
+//     <div v-if="isShow">
+//       <p>{{ info.name }}</p>
+//       <p>{{ info.sex }}</p>
+//       <p>{{ info.profile }}</p>
+//     </div>
+//   </div>
+// </template>
+
+// <script>
+// export default {
+//   name: 'InputForm',
+//   data() {
+//     return {
+//       isShow: false,
+//       info: {
+//         name: "",
+//         sex: "男性",
+//         profile: ""
+//       }
+//     }
+//   },
+//   watch: {
+//     info: {
+//       handler() {
+//         this.isShow = false
+//       },
+//       deep: true
+//     }
+//   },
+//   methods: {
+//     onClick() {
+//       this.isShow = true
+//     },
+//     OnInit() {
+//       const value = {
+//         name: "",
+//         sex: "",
+//         profile: ""
+//       } 
+//       this.$emit("input", value)
+//     }
+//   },
+//   props: {
+//     value: {
+//       type: Object,
+//       default: null
+//     }
+//   }
+// }
+// </script>
+
+// <style scoped>
+// .inputForm {
+//   margin: 10px;
+//   background: wheat;
+// }
+// </style>
+-->
