0f23ff2ee1c3d0155466a0bfeb1399a54a7b1139
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-01-31 12:47:57 +0900

    Calculation

---
+++ beginner_lesson/build/lesson/Lesson_Calc.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Lesson_Calc() {
+    const variable = 1;
+    const resultA = variable + 2;
+    const resultB = variable * 2;
+    const resultC = variable / 2;
+    console.log('resultA', resultA);
+    console.log('resultB', resultB);
+    console.log('resultC', resultC);
+    let resultD = 1;
+    resultD += 2;
+    let resultE = 1;
+    resultE *= 2;
+    let resultF = 1;
+    resultF /= 2;
+    console.log('resultD', resultD);
+    console.log('resultE', resultE);
+    console.log('resultF', resultF);
+}
+exports.Lesson_Calc = Lesson_Calc;
+Lesson_Calc();
+//# sourceMappingURL=Lesson_Calc.js.map
