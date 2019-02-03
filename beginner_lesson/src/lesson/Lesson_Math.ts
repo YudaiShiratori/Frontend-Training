65bf8ee271f951cbaea83e005982d62a54358555
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-02-03 19:56:22 +0900

    round ceil floor pow

---
+++ beginner_lesson/src/lesson/Lesson_Math.ts
@@ -0,0 +1,24 @@
+export function Lesson_Math() {
+  //四捨五入
+  const round1 = Math.round(5.71)
+  const round2 = Math.round(5.21)
+  console.log('round1', round1)
+  console.log('round2', round2)
+  //切り上げ
+  const ceil1 = Math.ceil(5.71)
+  const ceil2 = Math.ceil(4.21)
+  console.log('ceil1', ceil1)
+  console.log('ceil2', ceil2)
+  //切り捨て
+  const floor1 = Math.floor(5.78)
+  const floor2 = Math.floor(5.21)
+  console.log('ceil1', ceil1)
+  console.log('ceil2', ceil2)
+
+  //2乗
+  const pow = Math.pow(2, 4)
+  console.log('pow', pow)
+
+}
+
+Lesson_Math()
