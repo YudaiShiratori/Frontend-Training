4c3236017164cb6f9492338ea87e0b23812053a6
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-04-01 17:10:15 +0900

    TypaScript_tasks  1~3

--- beginner_lesson/build/app.js
+++ beginner_lesson/build/app.js
@@ -1,2 +1,37 @@
-console.log("Hello! world.");
+"use strict";
+// console.log("Hello! world.")
+Object.defineProperty(exports, "__esModule", { value: true });
+//task1
+function getHola() {
+    return 'hola';
+}
+exports.getHola = getHola;
+console.log(getHola());
+function getHolaWithDate() {
+    const date = new Date();
+    var hour = date.getHours();
+    if (5 <= hour && hour < 12) {
+        return 'おはよう';
+    }
+    else if (12 <= hour && hour < 18) {
+        return 'こんにちは';
+    }
+    else if (18 <= hour && hour < 24) {
+        return 'こんばんは';
+    }
+    else {
+        return '夜更かし';
+    }
+}
+exports.getHolaWithDate = getHolaWithDate;
+console.log(getHolaWithDate());
+function showHundred() {
+    let result = '';
+    for (let i = 0; i <= 100; i++) {
+        result += `${i} `;
+    }
+    console.log(result);
+}
+exports.showHundred = showHundred;
+console.log(showHundred());
 //# sourceMappingURL=app.js.map
--- beginner_lesson/src/app.ts
+++ beginner_lesson/src/app.ts
@@ -1,1 +1,32 @@
-console.log("Hello! world.")
+// console.log("Hello! world.")
+
+//tasks
+export function getHola() {
+  return 'hola'
+}
+console.log(getHola())
+
+export function getHolaWithDate() {
+  const date = new Date()
+  var hour = date.getHours()
+  if (5 <= hour && hour < 12) {
+    return 'おはよう'
+  } else if (12 <= hour && hour < 18) {
+    return 'こんにちは'
+  } else if (18 <= hour && hour < 24) {
+    return 'こんばんは'
+  } else {
+    return '夜更かし'
+  }
+}
+console.log(getHolaWithDate())
+
+export function showHundred() {
+  let result: string = ''
+  for(let i = 0; i<=100; i++) {
+    result += `${i} `
+  }
+  console.log(result)
+}
+console.log(showHundred())
+
